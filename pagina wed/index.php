00de25668692fd0cb7260bac1285571a2a93a4d7
Dariannygarcia <166844327+Dariannygarcia@users.noreply.github.com>
2024-04-15 15:47:55 -0600

    Add files via upload

---
+++ pagina wed/index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COCA - COLA</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="carta.css">
+    <link rel="icon" href="Coca-Cola_logo.svg.png"/>
+</head>
+<body>
+ <section>
+        <div class="circle"></div>
+        <header>
+         <a href="#"><img src="logo.png" alt="logo"></a>
+         <div class="toggle" oneclick="toggleMenu()"></div>
+         <ul class="navegation">
+            <li> <a href="#Inicio">Inicio</a></li>
+            <li> <a href="#Nosotros">Nosotros</a></li>
+            <li> <a href="#Productos">Productos</a></li>
+            <li> <a href="Contact-Form-HTML-CSS-master/index.html">Conctato</a></li>
+            <li> <a href="http://localhost/tienda/plomeria.php">Carrito</a></li>
+           
+         </ul>
+        </header>
+        <div class="content">
+            <div class="textbox">
+                <h2>Bienvenidos</h2>
+                <br>
+                <p>"Coca-Cola es  un símbolo de alegría y energía que ha refrescado
+                     momentos y avivado sonrisas en todo el mundo,
+                     trascendiendo sabores para convertirse en un impulso
+                      para la felicidad diaria."</p>
+                     <br>
+                     <a href="C:/Users/zulei/OneDrive/Escritorio/pagina%20wed/index.html#Inicio">Aprende mas</a>
+            </div>
+<div class="imgBox">
+    <swiper-container class="mySwiper" 
+    pagination="true" 
+    effect="coverflow" 
+    grab-cursor="true" 
+    loop="true"
+    centered-slides="true"
+    slides-per-view="auto"
+    coverflow-effect-rotate="0"
+    coverflow-effect-stretch="0"
+    coverflow-effect-depth="200"
+    coverflow-effect-modifier="1" 
+    coverflow-effect-slide-shadows="false">
+    <swiper-slide><img src="cocacola1.png" /></swiper-slide>
+    <swiper-slide><img src="cocacola2.png" /></swiper-slide>
+    <swiper-slide><img src="cocacola3.png" /></swiper-slide>
+    <swiper-slide><img src="cocacola4.png" /></swiper-slide>
+    <swiper-slide><img src="cocacola5.png" /></swiper-slide>
+    <swiper-slide><img src="cocacola6.png" /></swiper-slide>
+  </swiper-container>
+</div>
+</div>
+</section>
+<center><h3 id="Nosotros"></h3><center/>
+    <button class="buy-1">Nosotros</button>
+<div class="containe">
+    <div class="conte3">
+        <img src="" alt="" class="im">
+        <h3 class="h3">Historia</h3>
+        <p class="p">En 1869 John Stith Pemberton se mudó a Atlanta,
+            la capital del Georgia. En 1884 desarrolló y empezó a comercializar
+             una bebida que imitaba al Vino Mariani a la que la llamó Pemberton's 
+         French Wine Coca (Vino francés de Coca de Pemberton).
+     El 25 de noviembre de 1885 el condado de Fulton, cuya sede era Atlanta,
+    votó para aprobar la ley seca, a iniciarse el 1 de julio de 1886. 
+    Pemberton vio la noticia en la calle y empezó a desarrollar una fórmula
+    de su Vino francés de Coca, pero sin alcohol. En julio de 1886, Pemberton,
+    inventó la bebida original de Coca-Cola basada en la hoja de coca 
+  (Erythroxylum coca) y la nuez de cola (Cola acuminada) en agua
+carbonatada,10 para ofrecerse en venta en farmacias como una 
+bebida propiedades medicinales. Un año antes, en 1885, la bebida 
+española Nuez de Kola-coca —cuya única diferencia era que en lugar
+ de agua carbonatada usaba agua fresca fue presentada durante un concurso en Filadelfia.
+            
+            </p>
+            
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <div class="conte1">
+            <img src="" alt="" class="im">
+            <h2 class="h3">Visión</h2>
+            <br>
+            <p1 class="p1">Ser una empresa total de bebidas
+                 y líder en todos los mercados en donde opera</p1>
+            <ul class="ul">
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star"></i></li>
+            </ul>
+        </div>
+        <div class="conte1">
+            <img src="" alt="" class="im">
+            <h2 class="h2">Misión</h2>
+            <br>
+            <p1 class="p1"> Es ser un embotellador de excelencia bajo licencia de Coca-Cola,
+                 creando valor sustentable para sus constituyentes.</p1>
+            <ul class="ul">
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star checked"></i></li>
+                <li class="li"><i class="fa fa-star"></i></li>
+            </ul>
+        </div>
+        
+    <div class="conte1">
+        <img src="" alt="" class="im">
+        <h2 class="h2">Servicio</h2>
+        <br>
+        <p1 class="p1">Optimizar los estándares de servicio a los clientes para satisfacer de mejor
+            manera Sus necesidades y la de los consumidores.</p1>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+    </div>
+    <div class="conte1">
+        <img src="" alt="" class="im">
+        <h2 class="h2">Trabajo en Equipo</h2>
+        <br>
+        <p1 class="p1">Integrar orgánicamente la diversidad de estilos de trabajo existentes
+             a lo largo de toda la organización y aprovechar la riqueza de las diferencias individuales.</p1>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+    </div>
+    </div>
+</div>
+</div>
+  <center><h3 id="Productos"></h3><center/>
+    <button class="buy-1">Productos </button>
+  <div class="gallery">
+    <div class="conte">
+        <img src="cocacola1.png" alt="" class="im">
+        <h2 class="h2">Sabor Original</h2>
+        <p1 class="p1">Bebida refrescante aromatizada.</p1>
+        <h6 class="h6">RD$100.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3"> Comprar ahora </button>
+    </div>
+
+    <div class="conte">
+        <img src="cocacola3.png" alt="" class="im">
+        <h2 class="h2">Diet Coke</h2>
+        <p1 class="p1">Sabor refrescante de Coca-Cola Light</p1>
+        <h6 class="h6">RD$110.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3"> Comprar ahora </button>
+    </div>
+    <div class="conte">
+        <img src="cocacola5.png" alt="" class="im">
+        <h2 class="h2">Cherry Vanilla</h2>
+        <p1 class="p1">Una combinación única y deliciosa</p1>
+        <h6 class="h6">RD$200.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3"> Comprar ahora </button>
+    </div>
+    <div class="conte">
+        <img src="cocacola6.png" alt="" class="im">
+        <h2 class="h2"> Energeticas</h2>
+        <p1 class="p1">Bebida energética rica en cafeína con el sabor original de Coca-Cola.</p1>
+        <h6 class="h6">RD$125.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3">Comprar ahora</button>
+    </div>
+  <div class="conte">
+    <img src="cocacola2.png" alt="" class="im">
+    <h2 class="h2">Coca-Cola Naranja</h2>
+    <p1 class="p1">Bebida refrescante aromatizada</p1>
+    <h6 class="h6">RD$110.00</h6>
+    <ul class="ul">
+        <li class="li"><i class="fa fa-star checked"></i></li>
+        <li class="li"><i class="fa fa-star checked"></i></li>
+        <li class="li"><i class="fa fa-star checked"></i></li>
+        <li class="li"><i class="fa fa-star checked"></i></li>
+        <li class="li"><i class="fa fa-star"></i></li>
+    </ul>
+    <button class="buy-3"> Comprar ahora </button>
+    </div>
+    <div class="conte">
+        <img src="cocacola4.png" alt="" class="im">
+        <h2 class="h2">Coca-Cola con Coffee</h2>
+        <p1 class="p1">sabor a Coca-Cola impregnado con rico y lujoso sabor a café</p1>
+        <h6 class="h6">RD$110.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3"> Comprar ahora </button>
+    </div>
+    <div class="conte">
+        <img src="lime1234.jpg" alt="" class="im">
+        <h2 class="h2"> DIET Sabor Lima</h2>
+        <p1 class="p1">Sabor a Lima, Disfruta de un sabor único.</p1>
+        <h6 class="h6">RD$110.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3"> Comprar ahora </button>
+    </div>
+    <div class="conte">
+        <img src="Coca Cola Cherry Zero..png" alt="" class="im">
+        <h2 class="h2">Coca Cola Cherry Zero.</h2>
+        <p1 class="p1">Bebida refrescante</p1>
+        <h6 class="h6">RD$110.00</h6>
+        <ul class="ul">
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star checked"></i></li>
+            <li class="li"><i class="fa fa-star"></i></li>
+        </ul>
+        <button class="buy-3"> Comprar ahora </button>
+    </div>
+
+    <center><h3 class="buy-3">Marcas</h3><center/>
+        <br>
+        <br>
+        <button class="buy-4">bebidas</button>
+    <div class="containe">
+        <div class="conte3">
+            <img src="" alt="" class="im">
+            <button class="buy-4">bebidas</button>
+            <p class="">
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+<script>
+function toggleMenu(){
+    const menuToggle = document.querySelector('toggle');
+    const navegation = document.querySelector('navegation')
+    menuToggle.classList.toggle('active')
+    navegation.classList.toggle('active')
+
+}
+</script>
+</body>
+</html>
